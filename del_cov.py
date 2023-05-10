05daddef053ce1bcaaffdfdb01a417b4729c9a59
Prince Rathore <72643925+princeontheinternet@users.noreply.github.com>
2023-05-10 18:13:13 +0530

    Update del_cov.py

--- del_cov.py
+++ del_cov.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import csv
-df = pd.read_csv(r"C:\Users\neethu.nath.saama.co\Downloads\del_csv.csv")
+df = pd.read_csv(r"C:\Users\Downloads\del_csv.csv")
 df = df.reset_index(drop=True)
-df.to_csv(r"C:\Users\neethu.nath.saama.co\Downloads\del_pipe.csv",sep="|",quotechar='"',  quoting=csv.QUOTE_ALL,index=False)
+df.to_csv(r"C:\Users\Downloads\del_pipe.csv",sep="|",quotechar='"',  quoting=csv.QUOTE_ALL,index=False)
